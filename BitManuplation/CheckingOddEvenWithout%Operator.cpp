0d92948ff7fa3df2be9aa1611b21337197893c0f
Parth <20parthgupta03@gmail.com>
2024-01-12 15:46:12 +0530

    CheckingOddEvenWithout%Operator

---
+++ BitManuplation/CheckingOddEvenWithout%Operator.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if(n&1 == 1)
+    {
+        cout << "ODD" << endl;
+    }
+    else{
+        cout << "EVEN" << endl;
+    }
+    return 0;
+}
