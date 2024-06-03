26515d8d72521120cb65d33f0ad563cc41bd27ba
Parth <20parthgupta03@gmail.com>
2024-06-04 01:31:19 +0530

    4 5

--- CP-31/1000 rated/04 Olya and Game with Arrays.cpp
+++ CP-31/1000 rated/04 Olya and Game with Arrays.cpp
@@ -8,8 +8,12 @@ using namespace std;
 #define srt(v) sort(v.begin(),v.end())
 #define print_1D_arr(a,n)  for(ll i=0; i<n; i++),{cout<<a[i]<< ;} cout << endl;
 #define in_1D_arr(a,n)                     f(i,0,n){cin>>a[i];}
+#define all(v)  v.begin(),v.end()
+#define pb push_back
 typedef long long ll; 
 typedef vector<int> vi;
+typedef long long int  lli;
+const int INF = 1e9 + 7;
 bool all_same_element_in_arr(ll *arr, ll n){for(ll i=0; i<n-1; i++){if(arr[i]!=arr[i+1]){return false;}}return true;}
 void solve();
 //--------------------------------------------------MAIN---------------------------------------------------------------------------------------------------------------------------------------------------
@@ -22,53 +26,31 @@ int main() {
     return 0;
 }
 //--------------------------------------------------SOLVE---------------------------------------------------------------------------------------------------------------------------------------------------
-bool static mycomp(pair<ll, ll> p1, pair<ll, ll> p2)
-{
-    if (p1.first == p2.first)
-    {
-        return p1.second > p2.second;
-    }
-    else
-    {
-        return p1.first < p2.first;
-    }
-}
-void solve()
-{
-    ll n, p;
-    #include<vector> // Include the necessary header file
+void solve() {
+    int n;
+    cin >> n;
 
-    typedef pair<ll, ll> pll; // Define the type 'pll' as a pair of 'll' types
-    typedef vector<pll> vpll; // Define the type 'vpll' as a vector of 'pll' types
+    int global_min = INF;
+    vi second_min;
 
-    cin >> n >> p;
-    vector<ll> arr(n), brr(n);
-    vpll vp;
-    for (ll i = 0; i < n; i++)
+    while (n--)
     {
-        cin >> arr[i];
-    }
-    for (ll i = 0; i < n; i++)
-    {
-        cin >> brr[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        vp.push_back({min(brr[i], p), arr[i]});
-    }
-    if (n == 1)
-    {
-        cout << p << endl;
-        return;
-    }
-    sort(begin(vp), end(vp), mycomp);
-    ll ans = p;
-    ll rem = n - 1;
-    for (int i = 0; i < n - 1; i++)
-    {
-        ans += min(vp[i].second, rem) * vp[i].first;
-        rem -= min(rem, vp[i].second);
+        int m;
+        cin >> m;
+
+        vi el(m);
+        for(auto & e : el)
+        {
+            cin >> e;
+        }
+
+        int local_min = *min_element(all(el));
+        global_min = min(global_min, local_min);
+
+        el.erase(find(all(el), local_min));
+
+        second_min.pb(*min_element(all(el)));
     }
 
-    cout << ans << endl;
+    cout << accumulate(all(second_min), 0ll) + global_min - (*min_element(all(second_min))) << endl;
 }
